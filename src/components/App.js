1847507b7062499f738a1dca4d17d2e1bcbca278
Siddharth Hota <siddharthhota26@gmail.com>
2023-06-27 01:58:49 +0530

    array

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,16 @@ import './../styles/App.css';
 import axios from 'axios'
 
 const App = () => {
-  let [data, setData] = useState([]);
+  let [data, setData] = useState({});
   let [err, setErr] = useState('');
 
   useEffect(() => {
     (async () => {
       try {
         const response = await axios.get('https://dummyjson.com/products');
-        console.log(response.data.products);
-        setData(response.data.products);
-        if(!response.data.products){
+        console.log(response.data);
+        setData(response.data);
+        if (!response.data) {
           setErr('No data found');
           return;
         }
@@ -30,18 +30,16 @@ const App = () => {
     <div>
       {/* Do not remove the main div */}
       {
-        data.length ?
+        data ?
           <div>
             <h1>Data Fetched from API</h1>
             {
-              data.map(value => (
-                <pre>{JSON.stringify(value, null, 3)},</pre>
-              ))
+                <pre>{JSON.stringify(data, null, 3)},</pre>
             }
-          </div> :!err?
-          <p>Loading...</p>:<p>{err}</p>
+          </div> : !err ?
+            <p>Loading...</p> : <p>{err}</p>
       }
-      
+
     </div>
   )
 }
